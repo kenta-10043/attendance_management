932ab8a6ec5fc67513126bea985b7d1c193ac950
mori kenta <aquamoon1436@gmail.com>
2025-10-08 08:23:20 +0900

    一時保存

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Attendance;
+use App\Models\Application;
+use App\Models\BreakTime;
+use App\Models\User;
+use Carbon\Carbon;
+use App\Http\Requests\AttendanceRequest;
+
 
 class AdminController extends Controller
 {
@@ -11,8 +19,11 @@ class AdminController extends Controller
         return view('admin.admin_login');
     }
 
-    public function adminAttendanceList()
+    public function adminAttendanceList(Request $request)
     {
-        return view('admin.admin_attendance_list');
+        $date = $request->input('date') ? Carbon::parse($request->input('date'))->toDateString() : now()->toDateString();
+        $attendances = Attendance::whereDate('date', $date)->with('user', 'breakTimes')->get();
+
+        return view('admin.admin_attendance_list', compact('attendances', 'date'));
     }
 }
--- src/resources/views/admin/admin_attendance_list.blade.php
+++ src/resources/views/admin/admin_attendance_list.blade.php
@@ -7,5 +7,38 @@
 @endsection
 
 @section('content')
-    <div>main</div>
+    <h2 class="attendance__tittle">{{ $date }}の勤怠一覧</h2>
+
+    <div class="calendar">
+        <table class="calendar__content">
+            <tr>
+                <th class="attendance__item">名前</th>
+                <th class="attendance__item">出勤</th>
+                <th class="attendance__item">退勤</th>
+                <th class="attendance__item">休憩</th>
+                <th class="attendance__item">合計</th>
+                <th class="attendance__item">詳細</th>
+            </tr>
+
+
+            @foreach ($attendances as $attendance)
+                <tr>
+
+                    <td class="attendance__item__data">{{ optional($attendance->user)->name }} </td>
+                    <td class="attendance__item__data">
+                        {{ $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') : '' }} </td>
+                    <td class="attendance__item__data">
+                        {{ $attendance->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format('H:i') : '' }}
+                    </td>
+                    <td class="attendance__item__data"></td>
+                    <td class="attendance__item__data"></td>
+                    {{-- <form action="{{ route('attendance.detail', ['id' => $pendingAttendance->id]) }}" method="GET">
+                        <td class="attendance__item__data"> <button class="button__detail" type="submit">詳細</button>
+                        </td>
+                    </form> --}}
+
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
